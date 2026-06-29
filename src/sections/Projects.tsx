ecce5d0af60489f4bcfa9240498f2c03a2e4c33b
Tracy Qin <tracyjqin@gmail.com>
2026-06-30 00:02:57 +1000

    refactor: move proj data into a json

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import dataImport from "../projects.json";
+
+interface ProjectsItem {
+  name: string;
+  description: string;
+  link: string;
+}
+
+interface ProjectsData {
+  projects: ProjectsItem[];
+}
+
+const data = dataImport as unknown as ProjectsData;
+
 
 const Projects = () => {
   return (
+
     <div className="flex flex-col items-center gap-10 w-full min-h-[calc(100vh-4rem)] justify-center">
 
       <h1 className="text-white font-code text-2xl w-3/4 text-left max-w-4xl">
@@ -9,29 +24,31 @@ const Projects = () => {
       </h1>
 
       <div className="flex flex-col gap-6 items-center w-full">
-        <div className="flex flex-row bg-[#0E2654]/80 w-3/4 max-w-4xl p-4 border border-[#0E2654]/50 text-left rounded-md items-center justify-between">
 
-          <div className="flex flex-col gap-2">
-            <span className="text-blue-200 text-lg font-code">
-              Sonny Shelf (WIP)
-            </span>
+        {data.projects.map((proj: ProjectsItem, index: number) => (
+          <div className="flex flex-row bg-[#0E2654]/80 w-3/4 max-w-4xl p-4 border border-[#0E2654]/50 text-left rounded-md items-center justify-between" key={index}>
 
-            <span className="text-white text-md">
-              a sonny angel organiser to help me organise my collection
-            </span>
+            <div className="flex flex-col gap-2">
+              <span className="text-blue-200 text-lg font-code">
+                {proj.name}
+              </span>
 
+              <span className="text-white text-md">
+                {proj.description}
+              </span>
 
-
+            </div>
+            <div>
+              <button
+                onClick={() => window.location.href = proj.link}
+                className=" relative z-50 text-white text-sm px-3 py-1 rounded-md border border-white hover:bg-blue-900 transition cursor-pointer"
+              >
+                View here
+              </button>
+            </div>
           </div>
-          <div>
-            <button
-              onClick={() => window.location.href = "https://tracyjqin.github.io/sonny-shelf/"}
-              className=" relative z-50 text-white text-sm px-3 py-1 rounded-md border border-white hover:bg-blue-900 transition cursor-pointer"
-            >
-              View here
-            </button>
-          </div>
-        </div>
+        ))}
+
       </div>
     </div>
   );
